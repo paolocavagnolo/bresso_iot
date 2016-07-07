5428d51ef37ac783355b88106f07212bb13f8646
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-07-07 13:22:39 +0200

    go

--- script/plotEnergy.py
+++ script/plotEnergy.py
@@ -1,80 +1,134 @@
-import fileinput
-import time
-import logging
 import sys
+import time
 import os
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(message)s')
-logger = logging.getLogger()
+from datetime import datetime
+
+import plotly
+from plotly.graph_objs import *
 
 SYSTEM_PATH = '/home/pi/Documents/'
-# SYSTEM_PATH = '/Users/paolo/Documents/'
 PATH_KEYS = SYSTEM_PATH + 'keys.txt'
 fkey=open(PATH_KEYS,'r')
 fkeylines=fkey.readlines()
-GDRIVE_API_KEY = SYSTEM_PATH + fkeylines[1].split('\n')[0]
-MONGODB_CLIENT_MLAB = fkeylines[3].split('\n')[0]
-TELEGRAM_BRIDGE = SYSTEM_PATH + fkeylines[5].split('\n')[0]
-ENERGYLOG = SYSTEM_PATH + fkeylines[7].split('\n')[0]
-LOGCONFIG = SYSTEM_PATH + fkeylines[9].split('\n')[0]
-SYNC_TRIG = SYSTEM_PATH + fkeylines[13].split('\n')[0]
-PANEL_HTML = SYSTEM_PATH + fkeylines[15].split('\n')[0]
+ENERGY_FILE = SYSTEM_PATH + fkeylines[7].split('\n')[0]
 fkey.close()
 
-def data2web( data, a, b, c ):
-
-    processing_foo1s = False
-    last_row = "<tr><td>Data</td><td>A</td><td>B</td><td>C</td></tr>"
-    full = "<tr><td>" + data + "</td><td>" + str(a) + "</td><td>" + str(b) + "</td><td>" + str(c) + "</td></tr>"
-
-    for line in fileinput.input(PANEL_HTML, inplace=1):
-      if line.startswith(last_row):
-        processing_foo1s = True
-      else:
-        if processing_foo1s:
-          print full
-        processing_foo1s = False
-      print line,
+ERR_THS = 3
 
 def readFromFile():
-  buffer_file = ENERGYLOG
+  arduFile = ENERGY_FILE
   lines = []
-  with open(buffer_file, "r") as f:
+  with open(arduFile, "r") as f:
       lines = f.readlines()
-  # with open(buffer_file, "w") as f:
-  #     f.truncate()
-  f.close()
   lines = map(lambda x: x.rstrip(), lines)
   return lines
 
-
 while True:
-    if os.path.isfile(ENERGYLOG):
-        logger.debug("trovato file energia")
-        lines = readFromFile()
-        RM_FILE_COMMAND = "sudo rm " + ENERGYLOG
-        os.system(RM_FILE_COMMAND)
-        i=0
-        for line in lines:
-            data = lines[i].split(',')[0]
-            idphase = lines[i].split(',')[1]
-            count = lines[i].split(',')[2]
-            if idphase == 'a':
-                data2web(data,count,' ',' ')
-            elif idphase == 'b':
-                data2web(data,' ',count,' ')
-            elif idphase == 'c':
-                data2web(data,' ',' ',count)
+
+    lines = readFromFile()
+
+    xA = []
+    xB = []
+    xC = []
+
+    yA = []
+    yB = []
+    yC = []
+
+
+    for line in lines:
+        fase_tmp = line.split(',')[1]
+        if fase_tmp == 'a':
+            xA.append(line.split(',')[0])
+            yA.append(line.split(',')[2])
+        elif fase_tmp == 'b':
+            xB.append(line.split(',')[0])
+            yB.append(line.split(',')[2])
+        elif fase_tmp == 'c':
+            xC.append(line.split(',')[0])
+            yC.append(line.split(',')[2])
+        else:
+            print "errore, lettera non presente tra a,b e c"
+
+    print "inizio fase 2"
+    print "a"
+
+    dxA = []
+    i = 1
+    for x in xA[1:]:
+        if i==1:
+            d0 = datetime.strptime(xA[i-1], '%Y-%m-%d %H:%M:%S.%f')
+        else:
+            d0 = d1
+        d1 = datetime.strptime(xA[i], '%Y-%m-%d %H:%M:%S.%f')
+        delta = d1 - d0
+        delta_c = float(delta.seconds) + float(delta.microseconds)/1000000
+        dxA.append( 3600/(delta_c*1000)*10000 )
+        if delta_c < ERR_THS:
+            dxA.pop()
+            xA.pop(i)
+        else:
             i=i+1
 
-        logger.debug('Copy file to the apache server index.html')
-        copy_string = 'sudo cp ' + PANEL_HTML + ' /var/www/html/'
-        os.system(copy_string)
-        os.system('sudo rm /var/www/html/index.html')
-        os.system('sudo mv /var/www/html/panel.html /var/www/html/index.html')
+    dxB = []
+    i = 1
+    for x in xB[1:]:
+        if i==1:
+            d0 = datetime.strptime(xB[i-1], '%Y-%m-%d %H:%M:%S.%f')
+        else:
+            d0 = d1
+        d1 = datetime.strptime(xB[i], '%Y-%m-%d %H:%M:%S.%f')
+        delta = d1 - d0
+        delta_c = float(delta.seconds) + float(delta.microseconds)/1000000
+        dxB.append( 3600/(delta_c*1000)*10000 )
+        if delta_c < ERR_THS:
+            dxB.pop()
+            xB.pop(i)
+        else:
+            i=i+1
 
-        logger.debug('Starting from a new file')
-        new_file = 'sudo rm ' + PANEL_HTML
-        os.system(new_file)
-        copy_file = 'sudo cp ' + SYSTEM_PATH + 'techlab_iot/script/html/panel_init.html ' + PANEL_HTML
-        os.system(copy_file)
+    print "c"
+
+    dxC = []
+    i = 1
+    for x in xC[1:]:
+        if i==1:
+            d0 = datetime.strptime(xC[i-1], '%Y-%m-%d %H:%M:%S.%f')
+        else:
+            d0 = d1
+        d1 = datetime.strptime(xC[i], '%Y-%m-%d %H:%M:%S.%f')
+        delta = d1 - d0
+        delta_c = float(delta.seconds) + float(delta.microseconds)/1000000
+        dxC.append( 3600/(delta_c*1000)*10000 )
+        if delta_c < ERR_THS:
+            dxC.pop()
+            xC.pop(i)
+        else:
+            i=i+1
+
+
+    traceA = Scatter(
+        x=xA,
+        y=dxA
+    )
+    traceB = Scatter(
+        x=xB,
+        y=dxB
+    )
+    traceC = Scatter(
+        x=xC,
+        y=dxC
+    )
+
+
+    data = Data([traceA,traceB,traceC])
+    plotly.offline.plot(data, filename = 'energy.html')
+    commandString = "sudo cp energy.html /var/www/html/"
+    os.system(commandString)
+    commandString = "sudo rm /var/www/html/index.html"
+    os.system(commandString)
+    commandString = "sudo mv /var/www/html/energy.html /var/www/html/index.html"
+    os.system(commandString)
+
+    time.sleep(60)
