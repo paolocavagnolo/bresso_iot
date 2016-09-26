5b9190085cf9d69f001e6f8e66624f940ff6be8d
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-09-26 16:29:13 +0200

    go

--- script/plotEnergy.py
+++ script/plotEnergy.py
@@ -4,12 +4,13 @@ import os
 from datetime import datetime
 import dateutil.relativedelta as dateu
 
-SYSTEM_PATH = '/home/pi/Documents/'
-PATH_KEYS = SYSTEM_PATH + 'keys.txt'
-fkey=open(PATH_KEYS,'r')
-fkeylines=fkey.readlines()
-ENERGY_FILE = SYSTEM_PATH + fkeylines[7].split('\n')[0]
-fkey.close()
+# SYSTEM_PATH = '/home/pi/Documents/'
+# PATH_KEYS = SYSTEM_PATH + 'keys.txt'
+# fkey=open(PATH_KEYS,'r')
+# fkeylines=fkey.readlines()
+# ENERGY_FILE = SYSTEM_PATH + fkeylines[7].split('\n')[0]
+# fkey.close()
+ENERGY_FILE = "../log/log.log"
 
 def readFromFile():
   arduFile = ENERGY_FILE
@@ -22,58 +23,57 @@ def readFromFile():
   except:
       return lines
 
-while True:
+lines = readFromFile()
 
-    lines = readFromFile()
+xA = []
+xB = []
 
-    xA = []
-    xB = []
+yA = []
+yB = []
 
-    yA = []
-    yB = []
+for line in lines:
+    try:
+        fase_tmp = line.split(',')[1]
+    except:
+        fase_tmp = '4'
 
-    for line in lines:
-        try:
-            fase_tmp = line.split(',')[1]
-        except:
-            fase_tmp = '4'
+    if fase_tmp == '2':
+        xA.append(line.split(',')[0])
+        yA.append(line.split(',')[2])
+    elif fase_tmp == '3':
+        xB.append(line.split(',')[0])
+        yB.append(line.split(',')[2])
+    else:
+        print "errore, id non presente - solo 2 e 3"
 
-        if fase_tmp == '2':
-            xA.append(line.split(',')[0])
-            yA.append(line.split(',')[2])
-        elif fase_tmp == '3':
-            xB.append(line.split(',')[0])
-            yB.append(line.split(',')[2])
-        else:
-            print "errore, id non presente - solo 2 e 3"
+yyA = []
+yyB = []
 
-    yyA = []
-    yyB = []
+for y in yA:
+    yyA.append(3600000/float(y))
+for y in yB:
+    yyB.append(3600000/float(y))
 
-    for y in yA:
-        yyA.append(3600000/float(y))
-    for y in yB:
-        yyB.append(3600000/float(y))
 
-    totA = 0
-    for v in yyA[-10:]:
-        totA = totA + v
-    totA = totA / 10
 
-    totB = 0
-    for v in yyB[-10:]:
-        totB = totB + v
-    totB = totB / 10
+totA = 0
+for v in yyA[-10:]:
+    totA = totA + v
+totA = totA / 10
 
-    commandString = "sudo rm ~/Documents/bresso_iot/display/main.html"
-    os.system(commandString)
-    commandString = "sudo cp ~/Documents/bresso_iot/display/original.html ~/Documents/bresso_iot/display/main.html"
-    os.system(commandString)
-    
+totB = 0
+for v in yyB[-10:]:
+    totB = totB + v
+totB = totB / 10
 
-    commandString = "~/refresh.sh"
-    os.system(commandString)
-    time.sleep(60)
+# commandString = "sudo rm ~/Documents/bresso_iot/display/main.html"
+# os.system(commandString)
+# commandString = "sudo cp ~/Documents/bresso_iot/display/original.html ~/Documents/bresso_iot/display/main.html"
+# os.system(commandString)
+#
+# commandString = "~/refresh.sh"
+# os.system(commandString)
+# time.sleep(60)
 
 ########################
 
